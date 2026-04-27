fb1fc06d00c018e8b311f4887d13e72920cb57a6
Bastian Henneberg <bastian.henneberg@proton.me>
2026-04-27 11:25:50 +0200

    chore: sync live versions of hyprland, nvim options, ssh config

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -2,3 +2,17 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.relativenumber = false
+vim.opt.clipboard = "unnamedplus"
+
+vim.g.clipboard = {
+  name = "wl-clipboard",
+  copy = {
+    ["+"] = { "wl-copy", "--type", "text/plain" },
+    ["*"] = { "wl-copy", "--primary", "--type", "text/plain" },
+  },
+  paste = {
+    ["+"] = { "wl-paste", "--no-newline" },
+    ["*"] = { "wl-paste", "--no-newline", "--primary" },
+  },
+  cache_enabled = 1,
+}
